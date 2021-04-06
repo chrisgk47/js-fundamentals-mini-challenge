fdeb29ca9d7e64d268b4eb3885466837d1672fcb
Christopher Kim <chris.kim1990@gmail.com>
2021-04-06 11:49:18 -0400

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // *** Uncomment the lines below to test
 console.log("%cQuestion 1", "color: red") 
 let counter = 1;
-counter = 2;
+counter = 2; //re-assign values
 console.log(counter) 
 // // => 2
 console.log("%c----------", "color: red") 
@@ -13,8 +13,9 @@ console.log("%c----------", "color: red")
 // *** after testing, comment the line causing the error back in 
 // *** otherwise, the error will stop the rest of your code from running
 console.log("%cQuestion 2", "color: red") 
-const name = "Chris"
-console.log(name)
+const name = "Chris" //when using const, cant change/overwrite name. use const when you know for a fact 
+//the variable wont be changed
+console.log(name)//name is depracated. In the past in js library name was prob used. can be ignored
 // => "Chris"
 // console.log(name)
 // name = "Not Chris" 
@@ -27,11 +28,11 @@ console.log("%c----------", "color: red")
 // *** Uncomment the lines below to test
 console.log("%cQuestion 3", "color: red") 
 
-function drinkWater(current_thirst_level){
-  console.log("Man I sure am thirsty")
-  current_thirst_level -= 1
+function drinkWater(currentThirstLevel){ //use camelCase
+  console.log("Man I sure am thirsty") //console.log returns undefined. outputs to console in browser, puts returns in terminal
+  currentThirstLevel -= 1
   console.log("Ahh that hits the spot")
-  return current_thirst_level
+  return currentThirstLevel //js explicitly needs 'return' to return value in console
 }
 console.log(drinkWater(12))
 // // => "Man I sure am thirsty"
@@ -48,7 +49,7 @@ console.log("%cQuestion 4", "color: red")
 function sameSameButDifferent(num, maybeNum){
   if (num === maybeNum){
     return "same same"
-  } else if (num, maybeNum = "num"){
+  } else if (num == maybeNum ){  //}else if (num === parseInt(maybeNum)){ // == (js tries on its own if i can make both into same data type are they equal)
     return "same same (but different)"
   } else {
     return "different"
@@ -77,10 +78,12 @@ const student1 = { name: "Duane", grade: 88 }
 function updateGrade(student, grade){
   student.grade = grade
 }
-
-console.log(updateGrade(student1, 92))
-
+updateGrade(student1, 92)
 console.log(student1)
+
+// console.log(updateGrade(student1, 92))
+
+// console.log(student1)
 // // => { name: "Duane", grade: 92 }
 console.log("%c----------", "color: red")
 
@@ -99,7 +102,12 @@ const users = [
   }
 ]
 function print_name_and_phones(users){
-  
+  users.forEach(function(user)){ //function(user) in forEach() is a callback function // forEach takes a callback function
+    //always pass a callback function forEach
+    console.log(user.name)
+    console.log(`cell: ${user.phones.cell}`)
+    console.log(Office: ${`user.phones.office`})
+  }
 }
 printNameAndPhones(users)
 // // => "Duane"
@@ -128,6 +136,9 @@ function myMap(array, callback) {
 // *** Uncomment the lines below to test
 // console.log("%cCallbacks - Question 1", "color: red")
 
+function triple(num){
+  return num * 3
+}
 // console.log(myMap([1,2,3,4,5], triple)) 
 // // => [3,6,9,12,15]
 
@@ -138,6 +149,10 @@ function myMap(array, callback) {
 
 // ***** Callbacks - Question 2 *****
 // *** Uncomment the lines below to test
+
+function greet(name){
+  return `Hi, ${name}!`
+}
 // console.log("%cCallbacks - Question 2", "color: red")
 
 // console.log(myMap(["Raffy", "Chase"], greet)) 
@@ -168,7 +183,12 @@ const line = []
 
 // *** Uncomment the lines below to test
 // console.log("%cScope & Closures - Question 1", "color: red")
+let ticketNumber = 1
 
+function takeATicketNumber(){
+  console.log("Welcome, you are ticke number" + ticketNumber)
+  line.push(ticketNumber++)
+}
 // console.log(takeATicketNumber(line))
 // // => `Welcome. You are ticket number 1`
 
@@ -190,6 +210,19 @@ const line = []
 // *** Uncomment the lines below to test
 // console.log("%cScope & Closures - Question 2", "color: red")
 
+
+function ticketNumberGeneratorFunc(){
+
+  let ticketNum = 0
+
+  function takeATicketNumber(newLine){
+    ticketNum += 1
+    line.push(ticketNum)
+    return `Welcome, You are ticket number ${ticketNum}`
+  }
+  
+  return takeATicketNumber
+}
 // const newLine = []
 // const takeATicketNumberFunc = ticketNumberGeneratorFunc()
 // console.log(takeATicketNumberFunc(newLine))
